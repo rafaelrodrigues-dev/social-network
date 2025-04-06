17b4d69ac7379dc8863ba1573fce73d33eeb863e
Rafa0r7 <rafaelrodriguesrr06@gmail.com>
2025-04-05 21:55:17 -0300

    Added functionality to follow/unfollow profiles with control logic in the backend and form in the profile details template.

--- profiles/urls.py
+++ profiles/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'profiles'
 
 urlpatterns = [
-    path('<str:username>/',views.profile_detail,name='profile')
+    path('<str:username>/',views.profile_detail,name='profile'),
+    path('<str:username>/follow',views.follow,name='follow'),
 ]
--- profiles/views.py
+++ profiles/views.py
@@ -1,6 +1,26 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.urls import reverse
 from django.contrib.auth.decorators import login_required
+from django.http import Http404
 from .models import Profile
+
+@login_required(login_url='authors:login')
+def follow(request,username):
+    if not request.method == 'POST':
+        raise Http404()
+    
+    profile = get_object_or_404(Profile,user__username=username)
+    
+    if request.user == profile.user:
+        return Http404()
+    
+    if profile in request.user.profile.follow.all():
+        profile.followers.remove(request.user.profile)
+    else:
+        profile.followers.add(request.user.profile)
+    
+    return redirect(reverse('profiles:profile',kwargs={'username':username}))
+    
 
 @login_required(login_url='authors:login')
 def profile_detail(request,username):
