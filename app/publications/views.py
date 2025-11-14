90441ad8f47ab2ee32492930bd080d186f769033
Rafa0r7 <rafaelrodriguesrr06@gmail.com>
2025-11-14 16:52:03 -0300

    Updates the interface for the home, publication details, profile, edit profile, and add publication pages.

--- app/publications/views.py
+++ app/publications/views.py
@@ -1,12 +1,13 @@
 import os
-from django.shortcuts import render, redirect
-from publications.models import Publication,Comment
-from django.shortcuts import get_object_or_404
-from django.http import JsonResponse,Http404,HttpResponseForbidden
-from django.urls import reverse
+
 from django.contrib.auth.decorators import login_required
+from django.contrib.postgres.search import SearchRank, SearchVector
 from django.core.paginator import Paginator
-from django.contrib.postgres.search import SearchVector,SearchRank
+from django.http import Http404, HttpResponseForbidden, JsonResponse
+from django.shortcuts import get_object_or_404, redirect, render
+from django.urls import reverse
+
+from publications.models import Comment, Publication
 
 PER_PAGE = os.getenv('PER_PAGE',7)
 
@@ -44,6 +45,7 @@ def search(request):
     
     return render(request,'publications/pages/search_results.html',context={
         'page_obj':page_obj,
+        'query': query,
         'additional_query':'&q=' + query
     })
 
